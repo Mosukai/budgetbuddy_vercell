4e16bbff3a9e354cb09e457240ac84b6870b849f
Aiden Jeehoon On <jeehoono@uci.edu>
2024-06-11 20:11:48 -0700

    Bring

---
+++ src/Components/organisms/Header/header.jsx
@@ -0,0 +1,22 @@
+// src/components/Header.js
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+    <img src="logo.png" alt="" />
+
+    <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/expense">Expense</Link>
+        <Link to="/budget">Budget</Link>
+        <Link to="/account">Account</Link>
+        <Link to="/login">Log in</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
